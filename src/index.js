e6e00467f5739ef97f3b30663be9833d15427082
Gagan Mandal <mandalgagan4200@gmail.com>
2025-01-12 21:00:12 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ dotenv.config({
 
 
 connectDB()
-
-
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`sever is running at port : ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+})
 
 
 
